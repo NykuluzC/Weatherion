75f94f888b3453caa1a039e2d45418d14db5bbf5
NykuluzC <carlslasier1@gmail.com>
2025-03-11 05:25:32 +0800

    git init

---
+++ Weatherion/Models/Location.cs
@@ -0,0 +1,9 @@
+namespace Weatherion.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Weatherion/Models/News.cs
@@ -0,0 +1,14 @@
+namespace Weatherion.Models
+{
+    public class NewsApiResponse
+    {
+        public List<Article> Articles { get; set; }
+    }
+
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
